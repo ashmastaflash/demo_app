0387f62212f23090726c113da84b5e38a970f86f
Ash Wilson <ash.d.wilson@gmail.com>
2017-12-05 23:25:48 -0500

    chg: dev: Adding kill on critical feature. !minor

--- dondash/halo_util.py
+++ dondash/halo_util.py
@@ -43,6 +43,8 @@ class SecurityReporter(object):
             raw_scan_results.append(results)
         # Process and print scan results
         self.print_pretty_scans(raw_scan_results)
+        if [x for x in scan_types if x["critical_findings_count" != 0] and os.getenv("FAIL_ON_CRITICAL"):
+                print("CRITICAL FINDINGS! HARD_FAIL!!")
         return
 
     def print_pretty_scans(self, raw_scan_results):
